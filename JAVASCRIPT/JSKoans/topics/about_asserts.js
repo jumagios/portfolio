f8c96e7f44c87d526fd5b52292086e719a2670e5
jumagios <juangimenezos@hotmail.com>
2022-05-15 22:39:40 -0300

    agregando ejercicios para practicar conceptos de js, consistian en rellenar espacios en blanco

---
+++ JAVASCRIPT/JSKoans/topics/about_asserts.js
@@ -0,0 +1,23 @@
+// module("About Asserts (topics/about_asserts.js)");
+//const { ok, equal } = require('assert')
+const assert = require('assert')
+const { ok, equal } = assert
+// const { __, test } = require('../support/koans')
+const koans = require('../support/koans')
+// const __ = koans.__
+// const test = koans.test
+const { __ } = koans
+const { test } = koans
+
+
+koans.test("ok", () => {
+    assert.ok(true === true, 'what will satisfy the ok assertion?');
+})
+
+test("not ok", () => {
+    assert.ok( false === false, 'what is a false value?');
+})
+
+test("ok", () => {
+    assert.equal(2, 1 + 1, 'what will satisfy the equal assertion?');
+})
