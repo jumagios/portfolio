784debadf89848169b96cfa050dd54da8ec54047
Juan Gimenez <70039806+jumagios@users.noreply.github.com>
2023-04-24 23:54:55 -0300

    day 24 and organizing files

--- PYTHON/100 Day of Python/Intermediate/20 Day - Build the Snake Game Part 1 - Animation & Coordinates/snake_game/snake.py
+++ PYTHON/100 Day of Python/Intermediate/20 Day - Build the Snake Game Part 1 - Animation & Coordinates/snake_game/snake.py
@@ -37,15 +37,24 @@ class Snake:
             self.segments[seg_num].goto(new_x, new_y)
         self.segments[0].forward(MOVE_DISTANCE)
 
+    def reset(self):
+        for seg in self.segments:
+            seg.goto(1000, 1000)
+        self.segments.clear()
+        self.create_snake()
+        self.head = self.segments[0]
+
     def up(self):
         if self.head.heading() != DOWN:
             self.segments[0].setheading(UP)
     def down(self):
         if self.head.heading() != UP:
             self.segments[0].setheading(DOWN)
+
     def left(self):
         if self.head.heading() != RIGHT:
             self.segments[0].setheading(LEFT)
+
     def right(self):
         if self.head.heading() != LEFT:
             self.segments[0].setheading(RIGHT)
