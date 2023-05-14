ec635d3374fabbf3336189a6c2fad4d48137c579
Juan Gimenez <70039806+jumagios@users.noreply.github.com>
2023-05-14 04:03:17 -0300

    add some logic of exceptions

--- PYTHON/100 Day of Python/Intermediate/26 Day - List Comprehension and the NATO Alphabet/NATO-alphabet-start/main.py
+++ PYTHON/100 Day of Python/Intermediate/26 Day - List Comprehension and the NATO Alphabet/NATO-alphabet-start/main.py
@@ -1,35 +1,46 @@
 student_dict = {
-    "student": ["Angela", "James", "Lily"], 
+    "student": ["Angela", "James", "Lily"],
     "score": [56, 76, 98]
 }
 
-#Looping through dictionaries:
+# Looping through dictionaries:
 for (key, value) in student_dict.items():
-    #Access key and value
+    # Access key and value
     pass
 
 import pandas
+
 student_data_frame = pandas.DataFrame(student_dict)
 
-#Loop through rows of a data frame
+# Loop through rows of a data frame
 for (index, row) in student_data_frame.iterrows():
-    #Access index and row
-    #Access row.student or row.score
+    # Access index and row
+    # Access row.student or row.score
     pass
 
 # Keyword Method with iterrows()
 # {new_key:new_value for (index, row) in df.iterrows()}
 
-#TODO 1. Create a dictionary in this format:
+# TODO 1. Create a dictionary in this format:
 {"A": "Alfa", "B": "Bravo"}
 nato_phonetic = pandas.read_csv("nato_phonetic_alphabet.csv")
 
 new_dict = {row.letter: row.code for (index, row) in nato_phonetic.iterrows()}
 
-#TODO 2. Create a list of the phonetic code words from a word that the user inputs.
 
-user_input = input("Enter a word:").upper()
+# TODO 2. Create a list of the phonetic code words from a word that the user inputs.
 
-new_list = [new_dict[letter] for letter in user_input]
 
-print(new_list)
+def generate_phonetic():
+    user_input = input("Enter a word:").upper()
+
+    try:
+        new_list = [new_dict[letter] for letter in user_input]
+    except KeyError:
+        print("Sorry, use only letters for the alphabet")
+        generate_phonetic()
+    else:
+        print(new_list)
+
+
+generate_phonetic()
