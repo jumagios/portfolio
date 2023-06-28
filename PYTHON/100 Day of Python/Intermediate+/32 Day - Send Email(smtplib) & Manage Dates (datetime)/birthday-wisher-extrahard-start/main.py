a463d0d00d6199bc17d9fa5e2b738e38b5ff112c
Juan Gimenez <70039806+jumagios@users.noreply.github.com>
2023-06-27 23:57:54 -0300

    just finished the program of day 32 after a long time without coding because i was finishing my degree

--- PYTHON/100 Day of Python/Intermediate+/32 Day - Send Email(smtplib) & Manage Dates (datetime)/birthday-wisher-extrahard-start/main.py
+++ PYTHON/100 Day of Python/Intermediate+/32 Day - Send Email(smtplib) & Manage Dates (datetime)/birthday-wisher-extrahard-start/main.py
@@ -3,26 +3,72 @@
 # 1. Update the birthdays.csv
 
 # 2. Check if today matches a birthday in the birthdays.csv
-import datetime as dt
 import pandas as pd
+import datetime
+import random
+import os
+import smtplib
+from email.mime.multipart import MIMEMultipart
+from email.mime.text import MIMEText
 
-now = dt.datetime.now()
-actual_day = now.strftime("%d").replace('0', '')
-actual_month = now.strftime("%m").replace('0', '')
+# Read the CSV file
 df = pd.read_csv('birthdays.csv')
-all_days = df.day.to_list()
-all_months = df.month.to_list()
-for _ in range(len(all_days)):
-    if actual_day in all_days and actual_month in all_months:
-        print("si hay cumple")
-    else:
-        print("no hay")
 
+# Get the current date
+current_date = datetime.date.today()
+
+# Convert the year, month, and day columns to datetime format
+df['birthday'] = pd.to_datetime(df[['year', 'month', 'day']])
+
+# Filter the DataFrame to find matches
+matches = df[df['birthday'].dt.month == current_date.month]
+matches = matches[matches['birthday'].dt.day == current_date.day]
 
 # 3. If step 2 is true, pick a random letter from letter templates and replace the [NAME] with the person's actual name from birthdays.csv
+
+# Directory containing letter templates
+letter_template_dir = "letter_templates/"
+
+# List letter templates in the directory
+letter_templates = []
+for filename in os.listdir(letter_template_dir):
+    if filename.endswith(".txt"):
+        with open(os.path.join(letter_template_dir, filename), 'r') as file:
+            letter_templates.append(file.read())
+
+# Pick a random letter template
+letter_template = random.choice(letter_templates)
 
 # 4. Send the letter generated in step 3 to that person's email address.
 
 
+# Replace [NAME] with the person's actual name and send the email
+if not matches.empty:
+    for _, row in matches.iterrows():
+        name = row['name']
+        email = row['email']
+        personalized_letter = letter_template.replace('[NAME]', name)
 
+        # Set up the SMTP server
+        smtp_server = "smtp.gmail.com"
+        smtp_port = 587
+        smtp_username = "pedropascalito45@gmail.com"
+        smtp_password = "mtfiiycciewxrinw"
+
+        # Create the email message
+        message = MIMEMultipart()
+        message['From'] = smtp_username
+        message['To'] = email
+        message['Subject'] = 'Birthday Greetings'
+        message.attach(MIMEText(personalized_letter, 'plain'))
+
+        # Send the email
+        with smtplib.SMTP(smtp_server, smtp_port) as connection:
+            connection.starttls()
+            connection.login(smtp_username, smtp_password)
+            connection.send_message(message)
+
+        print(f"Email sent to {name} at {email}")
+else:
+    print("No birthdays today.")
 
