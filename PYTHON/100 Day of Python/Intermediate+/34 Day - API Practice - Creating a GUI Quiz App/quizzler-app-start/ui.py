5e159d254b441bb87d20e45775967a9402d5068a
Juan Gimenez <70039806+jumagios@users.noreply.github.com>
2024-02-17 08:03:52 -0300

    finish the quizzler app, give feedback to the player, keep score and fix bugs

--- PYTHON/100 Day of Python/Intermediate+/34 Day - API Practice - Creating a GUI Quiz App/quizzler-app-start/ui.py
+++ PYTHON/100 Day of Python/Intermediate+/34 Day - API Practice - Creating a GUI Quiz App/quizzler-app-start/ui.py
@@ -4,12 +4,6 @@ from quiz_brain import QuizBrain
 THEME_COLOR = "#375362"
 
 
-
-
-
-
-
-
 class QuizInterface:
 
     def __init__(self, quiz_brain: QuizBrain):
@@ -28,34 +22,45 @@ class QuizInterface:
             text="aca tenes papu")
         self.canvas.grid(row=1, column=0, columnspan=2, pady=50)
 
-        #Buttons
+        # Buttons
         cancel_image = PhotoImage(file="images/false.png")
         self.cancel_button = Button(image=cancel_image, highlightthickness=0, command=self.cancel_question)
         self.cancel_button.grid(row=2, column=0)
         confirm_image = PhotoImage(file="images/true.png")
-        self.confirm_button = Button(image=confirm_image, highlightthickness=0, command=confirm_question)
+        self.confirm_button = Button(image=confirm_image, highlightthickness=0, command=self.confirm_question)
         self.confirm_button.grid(row=2, column=1)
 
-        #Text
+        # Text
         self.score_label = Label(text="Score: 0", foreground="white", background=THEME_COLOR)
         self.score_label.grid(row=0, column=1)
 
-
         self.get_next_question()
-
-
-
 
         self.window.mainloop()
 
     def get_next_question(self):
-        q_text = self.quiz.next_question()
-        self.canvas.itemconfig(self.question_text, text=q_text)
+        self.canvas.config(bg="white")
+        if self.quiz.still_has_questions():
+            self.score_label.config(text=f'Score: {self.quiz.score}')
+            q_text = self.quiz.next_question()
+            self.canvas.itemconfig(self.question_text, text=q_text)
+        else:
+            self.canvas.itemconfig(self.question_text, text="You've reached the end of the quiz")
+            self.cancel_button.config(state="disabled")
+            self.confirm_button.config(state="disabled")
 
     def cancel_question(self):
-        self.quiz.check_answer("False")
+        self.give_feedback(self.quiz.check_answer("False"))
 
     def confirm_question(self):
-        self.quiz.check_answer("True")
+        is_right = self.quiz.check_answer("True")
+        self.give_feedback(is_right)
+
+    def give_feedback(self, is_right):
+        if is_right:
+            self.canvas.config(bg="green")
+        else:
+            self.canvas.config(bg="red")
+        self.window.after(1000, self.get_next_question)
 
 
