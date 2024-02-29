92dabca81e0d1fed96b81d83529a2010dc932375
Juan Gimenez <70039806+jumagios@users.noreply.github.com>
2024-02-29 14:15:15 -0300

    finish this project

--- PYTHON/100 Day of Python/Intermediate+/39 Day - Flight Deal Finder/flight-deals-start/data_manager.py
+++ PYTHON/100 Day of Python/Intermediate+/39 Day - Flight Deal Finder/flight-deals-start/data_manager.py
@@ -15,24 +15,20 @@ class DataManager:
     def get_destination_data(self):
         response = requests.get(url=SHEETY_ENDPOINT, headers=headers)
         data = response.json()
-        # pprint(data)
         self.destination_data = data["prices"]
-
         return self.destination_data
 
 
 
-    def update_destination_code(self):
+    def update_destination_codes(self):
         for city in self.destination_data:
             new_data = {
                 "price": {
                     "iataCode": city["iataCode"]
                 }
             }
-
             response = requests.put(
                 url=f"{SHEETY_ENDPOINT}/{city['id']}",
-                json=new_data,
-                headers=headers
+                json=new_data
             )
             print(response.text)
