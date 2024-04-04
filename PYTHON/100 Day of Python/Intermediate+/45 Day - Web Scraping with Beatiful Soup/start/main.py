8b0d8cc70463559b43b509aa28d638a42d45c8c1
Juan Gimenez <70039806+jumagios@users.noreply.github.com>
2024-04-04 20:23:56 -0300

    challenge

--- PYTHON/100 Day of Python/Intermediate+/45 Day - Web Scraping with Beatiful Soup/start/main.py
+++ PYTHON/100 Day of Python/Intermediate+/45 Day - Web Scraping with Beatiful Soup/start/main.py
@@ -1,15 +1,61 @@
-from bs4 import BeautifulSoup
-# import lxml
+# from bs4 import BeautifulSoup
+# # import lxml
+#
+# with open('website.html', 'r', encoding="utf8") as file:
+#     html_as_string = file.read()
+#
+# soup = BeautifulSoup(html_as_string, "html.parser")
+#
+# print(soup.title)
+# print(soup.title.string)
+#
+# print(soup.prettify())
+#
+# for item in soup:
+#     print(item.getText())
+#
+# all_anchor_tags = soup.findAll("a")
+#
+# for tag in all_anchor_tags:
+#     print(tag.getText())
+#     print(tag.get("href"))
+#
+# heading = soup.find(name="h1", id="name")
+#
+#
+# head_in_all = soup.find_all(class_="heading")
+# print(head_in_all)
+#
+# h3_head = soup.find_all("h3", class_="heading")
+# print(h3_head)
+#
+# company_url = soup.select(selector="p a")
+# print(company_url)
 
-with open('website.html', 'r', encoding="utf8") as file:
-    html_as_string = file.read()
+import requests
+import http.server
+import socketserver
 
-soup = BeautifulSoup(html_as_string, "html.parser")
+PORT = 8000  # Change port if needed
 
-print(soup.title)
-print(soup.title.string)
+class MockHandler(http.server.BaseHTTPRequestHandler):
+    def do_GET(self):
+        self.send_response(500)
+        self.send_header('Content-type', 'text/html')
+        self.end_headers()
+        self.wfile.write(b"Internal Server Error")
 
-print(soup.prettify())
+with socketserver.TCPServer(("", PORT), MockHandler) as httpd:
+    print("Serving at port", PORT)
+    # Run the mock server in a separate thread
+    import threading
+    server_thread = threading.Thread(target=httpd.serve_forever)
+    server_thread.start()
 
-for item in soup:
-    print(item.getText())
+    # Simulate client requests (replace with your actual server address)
+    for i in range(10000):
+        requests.get("http://localhost:8080")  # Replace with your server address
+
+    # Stop the mock server
+    httpd.shutdown()
+    server_thread.join()
